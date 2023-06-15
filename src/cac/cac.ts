c05ceb702e126d9b61a5be1c001ded7333bd8ea4
gumingWu <15626185054@163.com>
2023-06-16 00:58:23 +0800

    feat: cac demo

--- src/cac/cac.ts
+++ src/cac/cac.ts
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
+import cac from 'cac'
+import { version } from '../../package.json'
 
-console.log('cac')
+const cli = cac('cac')
+
+cli.command('dev', 'dev mode')
+  .action(() => {
+    console.log('im cac, dev mode')
+  })
+
+cli.command('build', 'build mode')
+  .action(() => {
+    console.log('im cac, build mode')
+  })
+
+cli.version(version)
+cli.help()
+cli.parse()
